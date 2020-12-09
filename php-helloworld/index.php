71941f460edf2076e35d93c14dc74ff4f36d7314
amaa1981 <amaa_1981@hotmail.com>
2020-12-09 16:59:28 +0300

    v1 commit

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-echo "<h2>Hello, From Red Hat to PetroRabigh! php version is <<h2>" . PHP_VERSION . "\n";
+echo "<h2>Hello, From Red Hat to PetroRabigh v1! php version is <<h2>" . PHP_VERSION . "\n";
 ?>
