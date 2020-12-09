c2d4f5726ef466c479093f995ef02ee132622ddd
amaa1981 <43506205+amaa1981@users.noreply.github.com>
2020-12-09 16:46:39 +0300

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,17 +1,4 @@
 <?php
 print "Hello, From Red Hat to PetroRabigh 
 ! php version is " . PHP_VERSION . "\n";
-// Create a blank image and add some text
-$im = imagecreatetruecolor(120, 20);
-$text_color = imagecolorallocate($im, 233, 14, 91);
-imagestring($im, 1, 5, 5,  'A Simple Text String', $text_color);
-
-// Set the content type header - in this case image/jpeg
-header('Content-Type: image/jpeg');
-
-// Output the image
-imagejpeg($im);
-
-// Free up memory
-imagedestroy($im);
 ?>
