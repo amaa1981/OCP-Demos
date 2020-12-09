7191da943317846f565896bc05a6a39494d2e672
amaa1981 <43506205+amaa1981@users.noreply.github.com>
2020-12-09 15:48:01 +0300

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, From Red Hat to PetroRabigh 
 ! php version is " . PHP_VERSION . "\n";
+<img src="http://open.thumbshots.org/image.pxf?url=http%3A%2F%2Fwww.hashbangcode.com%2F" alt="Site image" />
 ?>
