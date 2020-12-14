a8f55fa0de01a8f7e57b49ec843348ce422dfd6d
amaa1981 <43506205+amaa1981@users.noreply.github.com>
2020-12-14 09:15:37 +0300

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-echo "<h3>Hello, From Red Hat to PetroRabigh v1! php version is <<h3>" . PHP_VERSION . "\n";
+echo "<h3>Hello, From Red Hat to customer v1! php version is <<h3>" . PHP_VERSION . "\n";
 ?>
