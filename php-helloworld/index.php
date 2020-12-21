9d49689795c4d126352e3a0ff9c5be8f642ba3be
amaa1981 <amaa_1981@hotmail.com>
2020-12-21 09:56:06 +0300

    v1

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-echo "<h3>Hello, From Red Hat to customer v1! php version is <<h3>" . PHP_VERSION . "\n";
+echo "<h2>Hello, From Red Hat to customer! php version is <<h2>" . PHP_VERSION . "\n";
 ?>
