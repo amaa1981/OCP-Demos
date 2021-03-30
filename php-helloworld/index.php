c54771c7b96bf823adede3393cfd9e01af88a4b8
amaa1981 <43506205+amaa1981@users.noreply.github.com>
2021-03-30 09:39:09 +0300

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,8 +1,8 @@
 <?php
 echo "<h2>Hello, From Red Hat v5! php version is <<h2>" . PHP_VERSION . "\n";
-<ul>
-	<a href="#">
-		<img src="rh.png" alt>
-	</a>
-</ul>
+?>
+<?php
+<a href="#">
+	<img src="rh.png" alt>
+</a>
 ?>
