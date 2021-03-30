b1da969798e6ac966a17ed291eddef4069738779
amaa1981 <43506205+amaa1981@users.noreply.github.com>
2021-03-30 12:39:04 +0300

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -5,13 +5,6 @@
     </head>
     <body>
         <?php
-    header("Content-type: text/css; charset: UTF-8");
-
-   $brandColor = "#990000";
-   $linkColor = "#555555";
-   $CDNURL = "http://cdn.blahblah.net";
-?>
-        <?php
 echo "<h2>Hello from Red Hat v1</h2>"; 
 ?>
         <div>
