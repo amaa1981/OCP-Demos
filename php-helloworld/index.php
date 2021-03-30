e64105ae774d1cbfdd8301533a3d2fd1ae39d9ed
amaa1981 <43506205+amaa1981@users.noreply.github.com>
2021-03-30 12:23:52 +0300

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,17 +1,8 @@
-<?php
-echo "<h2>Hello, From Red Hat v5! php version is <<h2>" . PHP_VERSION . "\n";
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-        <title></title>
+        <title>Hello, From Red Hat v5!</title>
     </head>
-    <body>
-        <?php
-        echo ("Hello");
-        ?>
         <div>
             <img src="rh.png" alt="myPic" />
         </div>
